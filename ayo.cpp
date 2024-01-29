389b6790a34d7939c408a0e2ceec486621bfa844
Satria Khy <satria16alan@gmail.com>
2024-01-29 16:01:52 +0700

    fixed

--- ayo.cpp
+++ ayo.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main(){
 
-	cout <<setw(10)<<"ini adalah cara kita" ;
+	cout << setw(10) <<"ini adalah cara kita" <<"\n";
+	cout << setw(26) <<"tunjukan bahwa kau mampu"<<"\n";
+	cout << setw(30)<< "kau adalah perubahan"<<"\n";
 	
 	return 0;
 	
